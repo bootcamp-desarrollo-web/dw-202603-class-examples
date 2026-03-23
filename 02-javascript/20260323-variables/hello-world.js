52f48fe3c1313216bcbd0c903091f1d4b965eb74
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-03-23 20:08:57 +0100

    Variables en JS

---
+++ 02-javascript/20260323-variables/hello-world.js
@@ -0,0 +1,13 @@
+/**
+ * Nuestro primer fichero de JavaScript!
+ * Puedes ejecutarlo en varias maneras:
+ *  1. Desde consola: (en el mismo directorio del fichero)
+ *     node hello-world.js
+ *  2. Desde Visual studio desde menú lateral - "Run and Debug"
+ *  3. Desde consola de Visual Studio
+ * 
+ * Comprueba las tres formas para encontrar la que te resulta más cómoda.
+ */
+
+// Imprime un mensaje "Hello World"
+console.log("Hello World");
