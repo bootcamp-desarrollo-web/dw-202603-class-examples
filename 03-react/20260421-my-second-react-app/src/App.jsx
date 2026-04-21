5f4195cdca8d46749aa854a0499573158399d8ce
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-04-21 19:53:23 +0200

    Segunda app de React con primer componente

---
+++ 03-react/20260421-my-second-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import HelloWorld from "./HelloWorld"
+
+function App() {
+  return (
+    <div>
+      <h1 class="title">Hello World</h1>
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
---
+++ 03-react/20260421-my-second-react-app/src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+    return (
+        <div>Hello World 2</div>
+    )
+}
+
+// Exportar la función HelloWorld para que otros componentes
+// puedan importar este componente (que es equivalente a la función)
+export default HelloWorld
