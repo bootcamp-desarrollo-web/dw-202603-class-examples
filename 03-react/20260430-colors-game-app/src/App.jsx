9af188f02388346d4deac8e07f9e2514dfbecfe0
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-04-30 21:01:31 +0200

    Color game app - usar callback para que el componente hijo pueda avisar al padre

--- 03-react/20260430-colors-game-app/src/App.jsx
+++ 03-react/20260430-colors-game-app/src/App.jsx
@@ -2,11 +2,19 @@ import Button from "./components/Button"
 import buttonsList from "./data/buttonsList"
 
 function App() {
+  // Creamos una función (callback) para que el componente hijo la pueda llamar
+  function onBtnClick(colorHex) {
+    console.log("[App.jsx] Botton clicked: ", colorHex)
+  }
+
   return (
     <div className="container">
       { buttonsList.map((el, idx) => {
         return (
-          <Button key={idx} btnData={el} />
+          <Button key={idx}
+              btnData={el}
+              onBtnClickCallback={onBtnClick} // Aquí pasamos la función (callback) al hijo a través de la propiedad
+          />
         )
       }) }
     </div>
