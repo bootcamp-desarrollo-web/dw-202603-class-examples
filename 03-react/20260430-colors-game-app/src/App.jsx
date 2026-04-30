f38a3f62000fe5c4b90ed8bd69a8e1040e566c79
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-04-30 19:45:41 +0200

    Color game app - pasar array a un fichero externo

--- 03-react/20260430-colors-game-app/src/App.jsx
+++ 03-react/20260430-colors-game-app/src/App.jsx
@@ -1,24 +1,10 @@
 import Button from "./components/Button"
-
-const btnsArray = [
-  {
-    title: 'Red',
-    color: 'ff0000',
-  },
-  {
-    title: 'Blue',
-    color: '00ff00',
-  },
-  {
-    title: 'Green',
-    color: '0000ff',
-  },
-]
+import buttonsList from "./data/buttonsList"
 
 function App() {
   return (
     <div className="container">
-      { btnsArray.map((el, idx) => {
+      { buttonsList.map((el, idx) => {
         return (
           <Button key={idx} btnData={el} />
         )
