0001db6e2197cfa189881866d0bc05c36f14a435
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-04-30 18:40:25 +0200

    Color game app - paso 1

---
+++ 03-react/20260430-colors-game-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./components/Button"
+
+function App() {
+  return (
+    <div className="container">
+      <Button btnTitle={'Botón 1'} />
+      <Button btnTitle={'Botón 2'} />
+      <Button btnTitle={'Botón 3'} />
+      <Button btnTitle={'Botón 4'} />
+      <Button btnTitle={'Botón 5'} />
+    </div>
+  )
+}
+
+export default App
---
+++ 03-react/20260430-colors-game-app/src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button({btnTitle}) {
+  return (
+    <div className="button">
+        {btnTitle}
+    </div>
+  )
+}
+
+export default Button
