d93f7c7849e9f17ac6ab1324d3ad9a3c18fea744
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-05-07 19:54:09 +0200

    Añadir la funcionalidad que abre el modal con la foto seleccionada

--- practica-continua-galeria-de-fotos/20260506-photo-gallery/src/App.jsx
+++ practica-continua-galeria-de-fotos/20260506-photo-gallery/src/App.jsx
@@ -1,15 +1,32 @@
 import photosArr from "./data/photos"
 import PhotoHolder from "./components/PhotoHolder"
 import PhotoModal from "./components/PhotoModal"
+import { useState } from "react";
 
 function App() {
+  let [showModal, setShowModal] = useState(false)
+  let [selectedImgObj, setSelectedImgObj] = useState(null);
+
+  // ================================================================
+  // Esta callback se llama cuando el usuario selecciona una foto
   function onImgClick(imgObj) {
     console.log('[App:onImgClick]', imgObj)
+
+    // 1. actualizar la variable que contiene la foto seleccionada
+    //    (para mostrarla en el modal)
+    setSelectedImgObj(imgObj)
+
+    // 2. Mostrar el modal
+    //    --> Esto no funciona: showModal = true
+    setShowModal(true)
   }
+  // ================================================================
 
   return (
     <>
-    <PhotoModal imgObj={photosArr[0]} />
+    {/* Mostramos el modal según el valor de la variable showModal usando el operador ternario */}
+    { showModal ? <PhotoModal imgObj={selectedImgObj} /> : null }
+
     <div className="container">
       { photosArr.map((el, idx) => {
         return <PhotoHolder key={idx} imgObj={el} onImgClickCallback={onImgClick} />
