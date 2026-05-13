b15cbd7ea029fb116308f0e3ed091870c95f75f0
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-05-13 19:16:32 +0200

    Añadir hijos en el router que aparecen en la url del padre

--- 03-react/20260511-first-router/src/App.jsx
+++ 03-react/20260511-first-router/src/App.jsx
@@ -4,11 +4,13 @@ function App() {
 
   return (
     <>
-    <h1>App</h1>
+    <h1>** HEADER **</h1>
 
     <nav>
       <Link to="/">Home</Link> | <Link to='/about'>About</Link>
     </nav>
+
+    <hr />
 
     {/* Mostrar el contenido de los componentes hijos */}
     <Outlet />
--- 03-react/20260511-first-router/src/router.jsx
+++ 03-react/20260511-first-router/src/router.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import Team from './pages/Team.jsx'
 import TeamMember from './pages/TeamMember.jsx'
 
 const router = createBrowserRouter([
@@ -10,9 +12,20 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
         {
+            // Indicar que este hijo tiene que aparecer dentro del outlet con
+            // la misma url del padre
+            // (Es igual como poner: path: '')
+            index: true,  
+            element: <Home />,
+        },
+        {
             path: 'about',
             element: <About />,
             children: [
+                {
+                    path: '',
+                    element: <Team />
+                },
                 {
                     path: 'alonso',
                     element: <TeamMember memberName={'Alonso'} />
