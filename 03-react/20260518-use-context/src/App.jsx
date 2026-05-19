59687c7d4ebf6010a00a1aa923b1371eb5e93a75
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-05-19 19:11:05 +0200

    Pasar la función set del state dentro del contexto

--- 03-react/20260518-use-context/src/App.jsx
+++ 03-react/20260518-use-context/src/App.jsx
@@ -3,18 +3,19 @@ import ChildComponent from "./components/ChildComponent"
 import { UserContext } from "./contexts/UserContext"
 
 function App() {
+  console.log("****** Starting App component *****")
+
   const [username, setUsername] = useState("John Doe")
+  const [counter, setCounter ] = useState(1)
 
   return (
     <>
       <h1>Ejemplo <code>useContext</code> </h1>
       username (padre): {username}
 
-      <UserContext.Provider value={username}>
+      <UserContext.Provider value={{ username, setUsername, counter, setCounter }}>
         <ChildComponent />
       </UserContext.Provider>
-
-      <button onClick={() => { setUsername("John Doe 2") }}>Click</button>
     </>
   )
 }
--- 03-react/20260518-use-context/src/components/ChildComponent.jsx
+++ 03-react/20260518-use-context/src/components/ChildComponent.jsx
@@ -2,14 +2,23 @@ import { useContext } from "react"
 import { UserContext } from "../contexts/UserContext"
 
 function ChildComponent() {
-    const username = useContext(UserContext)
-    
-    return (
-        <>
-            <h3>Componente hijo</h3>
-            <p>username: {username}</p>
-        </>
-    )
+  const { username, setUsername, counter, setCounter } = useContext(UserContext)
+
+  return (
+    <>
+      <h3>Componente hijo</h3>
+      <p>username: {username}</p>
+      <p>counter: {counter}</p>
+
+      <button
+        onClick={() => {
+          setCounter(counter + 1), setUsername("John Doe " + counter)
+        }}
+      >
+        Click
+      </button>
+    </>
+  );
 }
 
-export default ChildComponent
+export default ChildComponent;
