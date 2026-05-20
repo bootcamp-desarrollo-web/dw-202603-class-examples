d7c607bbf29d5693569c1b82ab3f656ba29011e2
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-05-20 17:15:01 +0200

    Añadir funcionalidad de contador global usando contexto

--- 03-react/20260519-counter-context-app/src/App.jsx
+++ 03-react/20260519-counter-context-app/src/App.jsx
@@ -1,24 +1,31 @@
+import { useState } from "react"
 import Counter from "./components/Counter"
+import { GlobalCounterContext } from "./contexts/GlobalCounterContext"
 
 function App() {
+  const [globalCounter, setGlobalCounter] = useState(0)
+
   return (
     <>
-    <h1>Global counter: 4</h1>
-    <div className="box">
-      <Counter />
-    </div>
+    <h1>Global counter: {globalCounter}</h1>
 
-    <div className="box">
-      <Counter />
-    </div>
+    <GlobalCounterContext.Provider value={{globalCounter, setGlobalCounter}}>
+      <div className="box">
+        <Counter />
+      </div>
 
-    <div className="box">
-      <Counter />
-    </div>
+      <div className="box">
+        <Counter />
+      </div>
 
-    <div className="box">
-      <Counter />
-    </div>
+      <div className="box">
+        <Counter />
+      </div>
+
+      <div className="box">
+        <Counter />
+      </div>
+    </GlobalCounterContext.Provider>
     </>
   )
 }
--- 03-react/20260519-counter-context-app/src/components/Counter.jsx
+++ 03-react/20260519-counter-context-app/src/components/Counter.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react"
+import { GlobalCounterContext } from "../contexts/GlobalCounterContext"
+import { useContext } from "react"
 
 function Counter() {
   const [counter, setCounter] = useState(0)
 
+  const {globalCounter, setGlobalCounter} = useContext(GlobalCounterContext)
+
   function onBtnClick() {
     let newCounter = counter + 1
     setCounter(newCounter)
+
+    let newGlobalCounter = globalCounter + 1
+    setGlobalCounter(newGlobalCounter)
   }
 
   return (
