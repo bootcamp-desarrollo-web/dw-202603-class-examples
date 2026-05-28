13e894a2322c3f484870d978c47b5bb53fa5c037
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-05-28 20:01:06 +0200

    Añadir 'catch' para manejo de reject de la promesa

--- 04-apis/20260527-manejo-basico-promesas.js
+++ 04-apis/20260527-manejo-basico-promesas.js
@@ -1,9 +1,9 @@
 console.log('¡Comenzamos!')
 
-fetch('https://6a146e4f6c7db8aac054842f.mockapi.io/api/users')
+fetch('https://6a146e4f6c7db8aac054842f.mockapi.io/api/users/1')
     .then((response) => {
         console.log('¡Tengo la respuesta!')
-        // status más comunes:
+        // http status más comunes:
         // 200 - OK
         // 404 - not found
         // 500 - Server Error
@@ -23,5 +23,9 @@ fetch('https://6a146e4f6c7db8aac054842f.mockapi.io/api/users')
         console.log('-------------------------------------')
         console.log(data)
     })
+    .catch((err) => {
+        console.error('[ERROR] Se ha producido un error')
+    })
+    
 
 console.log('--- Fin ---')
