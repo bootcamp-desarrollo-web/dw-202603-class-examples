449176da3365da6a6de9f6b3254d1e91146967b2
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2026-06-01 19:38:30 +0200

    Añadir primera llamada a la api con useEffect

--- 04-apis/20260601-api-with-react/src/App.jsx
+++ 04-apis/20260601-api-with-react/src/App.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [apiData, setApiData] = useState()
+
+  console.log('==> apiData: ', apiData)
+
+  function fetchData() {
+    console.log('IN fetchData')
+    fetch('https://6a146e4f6c7db8aac054842f.mockapi.io/api/users')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+
+        } else {
+          throw new Error(`Status code ${response.status}`)
+        }
+
+      })
+      .then(data => {
+        console.log(data)
+        setApiData(data)
+      })
+      .catch(err => {
+        console.error(`[ERROR] ${err}`)
+      })
+  }
+
+  
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <>
-    <h1>First React API app.</h1>
+    <h3>First React API app.</h3>
+    {apiData ? 'algo' : 'nada'}
+    <p>{JSON.stringify(apiData)}</p>
     </>
   )
 }
